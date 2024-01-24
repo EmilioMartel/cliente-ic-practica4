fb77f5de22eecd70d06df7c832052ea14ac5f15a
EmilioMartel <emilio_martel@outlook.es>
2024-01-24 13:33:36 +0000

    datos de las placas con tarjetas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import placaImage from './assets/placa.webp';
+import './App.css'
 
 const SERVER_URL = 'http://localhost:3000'; // Ajusta la URL de tu servidor
 
 function App() {
-  const [socket, setSocket] = useState(null);
-  const [receivedMessage, setReceivedMessage] = useState('');
+  const [dataArray, setDataArray] = useState([]); // Nuevo estado para almacenar el array de objetos JSON
 
   useEffect(() => {
     const socketConnection = io(SERVER_URL);
@@ -19,9 +20,13 @@ function App() {
     };
 
     const handleMqttMessage = (message) => {
-      console.log("hadleMqttMessage", message)
-      setReceivedMessage(message);
-    };
+      try {
+        const parsedMessage = JSON.parse(message);
+        setDataArray(parsedMessage)
+      } catch (error) {
+        console.error('Error al parsear el mensaje como objeto JSON:', error);
+      }
+    }
 
     const handleError = (error) => {
       console.error('Error en la conexión del socket:', error);
@@ -32,29 +37,35 @@ function App() {
     socketConnection.on('error', handleError);
     socketConnection.on('messageFromReactWeb', handleMqttMessage);
 
-    setSocket(socketConnection);
-
     return () => {
       if (socketConnection.connected) {
         socketConnection.disconnect();
       }
-      setSocket(null);
     };
-  }, []);
+  }, [dataArray]);
 
-  const sendMessage = () => {
-    if (socket) {
-      const message = 'Mensaje desde el cliente de react';
-      socket.emit('messageFromReactWeb', message);
-      console.log(`Mensaje enviado al servidor: ${message}`);
-    }
-  };
 
   return (
     <div className="App">
-      <h1>React con Socket.IO</h1>
-      <p>Último mensaje recibido: {receivedMessage}</p>
-      <button onClick={sendMessage}>Enviar Mensaje</button>
+      <h1>Gestor de Residuos</h1>
+
+      {/* Genera dinámicamente las tarjetas según el tamaño del array */}
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {dataArray.map((item) => (
+          <div key={item.id} className="col">
+            <div className="card">
+              {/* Puedes ajustar la lógica para cargar imágenes según los datos del array */}
+              <img src={placaImage} className="card-img-top" alt="Card Image" />
+              <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>                
+                <p className="card-text">Status: {(item.status) ? '✅' : '🚩'} </p>
+                <p className="card-text">Capacidad: {item.capacidad}</p>
+                <p className="card-text">Coordenadas: {item.coordenadas}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
